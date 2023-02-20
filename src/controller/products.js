c9ac54cc5545bc07a921a3c3fc4fb7d355bb76e7
nampham <nampham18082003@gmail.com>
2023-02-20 16:03:23 +0700

    a

--- src/controller/products.js
+++ src/controller/products.js
@@ -18,12 +18,10 @@ export const getOne = async (req, res) => {
   try {
     const _id = { _id: req.params.id };
     const data = await get(_id);
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
     console.log(error)
-    res.status(400).json({
-      message: "Không ìm thấy bài viết"
-    })
+    return res.send("Không tìm thấy movie " + _id)
   }
 }
 
