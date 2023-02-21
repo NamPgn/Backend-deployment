48c668a18957b8c3a51b3b2353be9172aa626607
nampham <nampham18082003@gmail.com>
2023-02-21 23:15:26 +0700

    a

--- src/controller/products.js
+++ src/controller/products.js
@@ -35,7 +35,7 @@ export const addProduct = async (req, res) => {
       category: category,
       price: price,
       descriptions: descriptions,
-      image: `https://test-19k8.onrender.com/public/product/${filename}`,
+      image: `https://test-19k8.onrender.com/product/${filename}`,
       linkVideo: linkVideo,
       seri: seri,
       copyright: copyright,
