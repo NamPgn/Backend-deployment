7535d3c19db89d3677075f4fb15ef0a994de8376
nampham <nampham18082003@gmail.com>
2023-02-25 21:01:13 +0700

    update

--- src/routes/trailerHomePage.js
+++ src/routes/trailerHomePage.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { editTrailerHomePageUrlController, getUrlTrailerControllers } from "../controller/trailerHomePageController";
+import { editTrailerHomePageUrlController, getTrailerController, getUrlTrailerControllers } from "../controller/trailerHomePageController";
 
 const router = express.Router()
 
 router.get('/trailerUrl', getUrlTrailerControllers);
 router.put('/trailerUrl/:id', editTrailerHomePageUrlController);
+router.get('/trailerUrl/:id',getTrailerController);
 
 export default router;
